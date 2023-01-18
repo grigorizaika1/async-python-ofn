5b732e1111a78df4d72d9c53bd40e1c03f456214
grigorizaika1 <74550093+grigorizaika1@users.noreply.github.com>
2023-01-18 17:41:02 +0100

    Modified context send to use only one argument. I've no idea what nthe signature is, it's not documented, so might as well start there.

--- src/main.py
+++ src/main.py
@@ -2,4 +2,8 @@ import functions_framework
 
 @functions_framework.event
 def hello_world(event, context):
-    return "Hello World!"
+    # context.logger.info("Event ID: %s", context.event_id) # this is a suggestion from copilot
+    context.send("1. Hello World!")
+    return "2. Hello World!"
+
+
