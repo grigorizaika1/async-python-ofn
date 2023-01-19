070feee9139bac8702b1935c72d40af6b02cddd8
grigorizaika1 <74550093+grigorizaika1@users.noreply.github.com>
2023-01-19 20:26:36 +0100

    added function that sends message to mqtt to verify if the cloud function is getting triggered at all. deleted the event decorator

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import functions_framework
 from openfunction.function_runtime import OpenFunctionRuntime
+from mqtt_check import message_mqtt
 
-@functions_framework.event
+# @functions_framework.event
 def hello_world(event, context):
-    runtime = OpenFunctionRuntime.parse(context)
-    runtime.send("Hello World!", "async-output")
+    message_mqtt()
+
+    # runtime = OpenFunctionRuntime.parse(context)
+    # runtime.send("Hello World!", "async-output")
 
     return "2. Hello World!"
 
