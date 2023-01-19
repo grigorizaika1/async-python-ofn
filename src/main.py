1d97e17ccdf3d5fece3cb3168c92728bfc7b47e8
grigorizaika1 <74550093+grigorizaika1@users.noreply.github.com>
2023-01-19 20:47:09 +0100

    Added event decorator

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import functions_framework
 from openfunction.function_runtime import OpenFunctionRuntime
 from mqtt_check import message_mqtt
 
-# @functions_framework.event
+@functions_framework.event
 def hello_world(event, context):
     message_mqtt()
 
