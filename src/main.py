0b3794500b63414dbc54b6c0e0c5d7ac6b069b93
grigorizaika1 <74550093+grigorizaika1@users.noreply.github.com>
2023-01-22 20:52:04 +0100

    Changed the function to a sync one

--- src/main.py
+++ src/main.py
@@ -1,13 +1,29 @@
 import functions_framework
+from functions_framework.event_conversion import marshal_background_event_data
+from functions_framework.background_event import BackgroundEvent
 from openfunction.function_runtime import OpenFunctionRuntime
+from openfunction.function_context import FunctionContext
+from openfunction.dapr_output_middleware import dapr_output_middleware
+
 from mqtt_check import message_mqtt
 
-@functions_framework.event
-def hello_world(event, context):
+# @functions_framework.event
+# def hello_world(event, context):
+#     message_mqtt()
+
+#     # runtime = OpenFunctionRuntime.parse(context)
+#     # runtime.send("Hello World!", "async-output")
+
+#     return "2. Hello World!"
+
+@functions_framework.http
+def hello_world(request):
     message_mqtt()
 
-    # runtime = OpenFunctionRuntime.parse(context)
-    # runtime.send("Hello World!", "async-output")
+    event_data = marshal_background_event_data(request)
+    event = BackgroundEvent(**event_data)
+    context = FunctionContext(**event.context)
+    runtime = OpenFunctionRuntime.parse(context)
+    runtime.send("Hello World!!!", "async-output")
 
     return "2. Hello World!"
-
