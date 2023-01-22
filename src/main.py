b250bd9571274d9dd96b88646d028b6cbae640f3
grigorizaika1 <74550093+grigorizaika1@users.noreply.github.com>
2023-01-22 21:57:41 +0100

    -

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-import functions_framework
-from functions_framework.event_conversion import marshal_background_event_data
-from functions_framework.background_event import BackgroundEvent
-from openfunction.function_runtime import OpenFunctionRuntime
-from openfunction.function_context import FunctionContext
-from openfunction.dapr_output_middleware import dapr_output_middleware
+# import functions_framework
+# from functions_framework.event_conversion import marshal_background_event_data
+# from functions_framework.background_event import BackgroundEvent
+# from openfunction.function_runtime import OpenFunctionRuntime
+# from openfunction.function_context import FunctionContext
+# from openfunction.dapr_output_middleware import dapr_output_middleware
 
-from mqtt_check import message_mqtt
+# from mqtt_check import message_mqtt
 
 # @functions_framework.event
 # def hello_world(event, context):
@@ -18,12 +18,12 @@ from mqtt_check import message_mqtt
 
 # @functions_framework.http
 def hello_world(request):
-    message_mqtt()
+    # message_mqtt()
 
-    event_data = marshal_background_event_data(request)
-    event = BackgroundEvent(**event_data)
-    context = FunctionContext(**event.context)
-    runtime = OpenFunctionRuntime.parse(context)
-    runtime.send("Hello World!!!", "async-output")
+    # event_data = marshal_background_event_data(request)
+    # event = BackgroundEvent(**event_data)
+    # context = FunctionContext(**event.context)
+    # runtime = OpenFunctionRuntime.parse(context)
+    # runtime.send("Hello World!!!", "async-output")
 
     return "2. Hello World!"
