fea54b8758a40e8587ad949c79b4f6621ac9689c
grigorizaika1 <74550093+grigorizaika1@users.noreply.github.com>
2023-01-23 18:06:25 +0100

    -

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import json
 import functions_framework
-from functions_framework.event_conversion import marshal_background_event_data
-from functions_framework.background_event import BackgroundEvent
-from openfunction.function_runtime import OpenFunctionRuntime
-from openfunction.function_context import FunctionContext
-from openfunction.dapr_output_middleware import dapr_output_middleware
+# from functions_framework.event_conversion import marshal_background_event_data
+# from functions_framework.background_event import BackgroundEvent
+# from openfunction.function_runtime import OpenFunctionRuntime
+# from openfunction.function_context import FunctionContext
+# from openfunction.dapr_output_middleware import dapr_output_middleware
 
 from mqtt_check import message_mqtt
 
