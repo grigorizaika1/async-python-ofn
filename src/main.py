b92e8b2b40cd6c30e0c5e7ad9a12f561b5fb81d5
grigorizaika1 <74550093+grigorizaika1@users.noreply.github.com>
2023-01-27 11:28:56 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
+import json
+
 def hello_world(request):
-    return {
+    response_data = {
         "message": "hello, world",
         "form": request.form,
         "data": request.data,
         "args": request.args
     }
+    
+    return json.dumps(response_data)
